57355f418a75442dc569b8ad5b85807e0aaee9b3
SchiwftyCoder <adomprince@gmail.com>
2023-12-08 16:37:09 -0500

    removed date-and-time module as it is no longer needed

--- todolist_capstone/index.js
+++ todolist_capstone/index.js
@@ -1,10 +1,16 @@
 import express from "express";
-import bodyParser from "body-parser";
-import dateTime from "date-and-time"
+import bodyParser from "body-parser"; 
 
 const app = express();
 const port = 3000;
 const taskList = []
+// let now = new Date()
+// let today = new Date();
+// let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// let CurrentDateTime = date+ ' ' + time;
+
+// console.log(CurrentDateTime)
 
 //log input from user and make it available to the response object in the middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //middleware call to serve static pages from the public folder
 app.use(express.static("public"))
 
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {   
     res.render("index.ejs", {
         "task": taskList, 
      })
